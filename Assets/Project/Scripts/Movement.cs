140f972b02e463102908ae5321081309ffc78631
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-04-11 11:58:22 -0700

    player controller moves and lookes witha a clamped Vertical rotation, im trying to get the object to move only in the X axis tho and not the Y

---
+++ Assets/Project/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    Vector3 forwardDir;
+    Vector3 rightDir;
+    Vector3 myForwardDir;
+
+    //Vector3 CameraYrot;
+
+    Transform Camera;
+    void Start ()
+    {
+       // Debug.Log(transform.childCount);
+       // CameraX = this.gameObject.transform.GetChild(0);
+        Camera = GetComponentInChildren<Camera>().transform;
+       // Debug.Log(CameraX.transform.childCount);
+        rb = GetComponent<Rigidbody>();
+	}
+	
+    void FixedUpdate()
+    {
+        //Debug.Log((forwardDir + rightDir) * speed );
+        //rb.AddForce((forwardDir + rightDir) * speed );
+        rb.MovePosition(transform.position + (forwardDir + rightDir) * speed );
+    }
+
+	void Update ()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        forwardDir = Camera.transform.forward * vert;
+        rightDir = Camera.transform.right * horz;
+
+        gameObject.transform.rotation = Camera.transform.rotation;
+
+
+       // Debug.Log(horz + " " + vert);
+        //GetComponent<Rigidbody>().MovePosition(transform.position + dirVector * Time.deltaTime);
+	}
+}
---
+++ Assets/Project/Scripts/PointToNearestTransform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointToNearestTransform : MonoBehaviour
+{
+    public Transform[] targets;
+
+
+    void Update()
+    {
+        try
+        {
+            GetHeadingForNearestTarget(targets);
+        }
+        catch (System.ArgumentNullException e)
+        {
+            Debug.LogError("failed to find new heading");
+        }
+    }
+
+
+    Vector3 GetHeadingForNearestTarget(Transform[] potentialTargets)
+    {
+        if (potentialTargets == null) { throw new System.ArgumentNullException("PotentialTargets"); }
+        if (potentialTargets.Length == 0) { throw new System.ArgumentException("Need at least one target first"); }
+        Transform nearestTransform = potentialTargets[0];
+        float distance = Vector3.Distance(transform.position, potentialTargets[0].transform.position);
+
+        for (int i = 0; i < potentialTargets.Length; ++i)
+        {
+            float tempDist = Vector3.Distance(transform.position, potentialTargets[i].transform.position);
+            if (tempDist < distance)
+            {
+                distance = tempDist;
+                nearestTransform = potentialTargets[i];
+            }
+        }
+        return (nearestTransform.position - transform.position).normalized;
+    }
+}
